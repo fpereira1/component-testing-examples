92d8435686cab934002db4d309158a38c63ee5d8
Filype Pereira <pereira.filype@gmail.com>
2025-09-27 09:28:49 +1200

    updates based on docs

--- react-typescript-vite/src/tests/HelloWorld.test.tsx
+++ react-typescript-vite/src/tests/HelloWorld.test.tsx
@@ -1,17 +1,17 @@
 import 'react'
 import { $, expect } from '@wdio/globals'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import App from '../App'
 
 describe('React Component Testing', () => {
   it('increments value on click', async () => {
     // The render method returns a collection of utilities to query your component.
-    const { getByText } = render(<App />)
+    render(<App />)
 
     // getByText returns the first matching node for the provided text, and
     // throws an error if no elements match or if more than one match is found.
-    const btn = getByText('count is 0')
-    const button = await $(btn)
+    const btn = screen.getByText('count is 0')
+    const button = $(btn)
 
     // Dispatch a native click event to our button element.
     await button.click()
