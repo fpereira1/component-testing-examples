d4ae3675ee8e04f26cef39fd420234eb74cd6884
Filype Pereira <pereira.filype@gmail.com>
2025-09-27 14:21:24 +1200

    update HelloWorld.test.tsx

--- react-typescript-vite/src/tests/HelloWorld.test.tsx
+++ react-typescript-vite/src/tests/HelloWorld.test.tsx
@@ -1,6 +1,6 @@
 import 'react'
 import { $, expect } from '@wdio/globals'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from '../App'
 
 describe('React Component Testing', () => {
@@ -8,10 +8,7 @@ describe('React Component Testing', () => {
     // The render method returns a collection of utilities to query your component.
     render(<App />)
 
-    // getByText returns the first matching node for the provided text, and
-    // throws an error if no elements match or if more than one match is found.
-    const btn = screen.getByText('count is 0')
-    const button = $(btn)
+    const button = $('button*=count is')
 
     // Dispatch a native click event to our button element.
     await button.click()
